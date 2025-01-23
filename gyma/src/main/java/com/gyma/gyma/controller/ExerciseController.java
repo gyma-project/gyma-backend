649e2f3ea4d53fe602e5fe9f647abc66484fc31a
robert <dinizrobert2002@gmail.com>
2025-01-22 23:41:37 -0300

    feat: testes para exercises

--- gyma/src/main/java/com/gyma/gyma/controller/ExerciseController.java
+++ gyma/src/main/java/com/gyma/gyma/controller/ExerciseController.java
@@ -6,6 +6,7 @@ import com.gyma.gyma.model.Exercise;
 import com.gyma.gyma.service.ExerciseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,8 +33,18 @@ public class ExerciseController {
     @PostMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('TRAINER')")
     @Operation(summary = "Criar", description = "Criar exercício.")
-    public ResponseEntity<ExerciseDTO> criar(@RequestBody ExerciseDTO exerciseDTO){
+    public ResponseEntity<ExerciseDTO> criar(@Valid @RequestBody ExerciseDTO exerciseDTO){
         return ResponseEntity.ok(exerciseService.salvar(exerciseDTO));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('TRAINER')")
+    @Operation(summary = "Editar", description = "Editar exercício.")
+    public ResponseEntity<ExerciseDTO> editar(
+            @PathVariable Integer id,
+            @RequestBody ExerciseDTO exerciseDTO
+    ){
+        return ResponseEntity.ok(exerciseService.editar(id, exerciseDTO));
     }
 
     @DeleteMapping
--- gyma/src/main/java/com/gyma/gyma/service/ExerciseService.java
+++ gyma/src/main/java/com/gyma/gyma/service/ExerciseService.java
@@ -1,6 +1,7 @@
 package com.gyma.gyma.service;
 
 import com.gyma.gyma.controller.dto.ExerciseDTO;
+import com.gyma.gyma.exception.ResourceNotFoundException;
 import com.gyma.gyma.mappers.ExerciseMapper;
 import com.gyma.gyma.model.Exercise;
 import com.gyma.gyma.repository.ExerciseRepository;
@@ -20,6 +21,20 @@ public class ExerciseService {
 
     public ExerciseDTO salvar(ExerciseDTO exerciseDTO){
         Exercise exercise = new Exercise();
+        exercise.setName(exerciseDTO.name());
+        exercise.setAmount(exerciseDTO.amount());
+        exercise.setMuscleGroup(exerciseDTO.muscleGroup());
+        exercise.setRepetition(exerciseDTO.repetition());
+
+        exerciseRepository.save(exercise);
+
+        return exerciseMapper.toDTO(exercise);
+    }
+
+    public ExerciseDTO editar(Integer id, ExerciseDTO exerciseDTO){
+        Exercise exercise = exerciseRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exercício não encontrado com o id: " + id));
+
         exercise.setName(exerciseDTO.name());
         exercise.setAmount(exerciseDTO.amount());
         exercise.setMuscleGroup(exerciseDTO.muscleGroup());
