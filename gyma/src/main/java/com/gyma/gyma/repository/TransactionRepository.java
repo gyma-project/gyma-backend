7dfa1ef6c1d2fd3e34c931ec11efb950ce7cc535
robert <dinizrobert2002@gmail.com>
2025-03-06 21:53:12 -0300

    refact: ajustando envio de relatórios semanais

--- gyma/src/main/java/com/gyma/gyma/repository/TransactionRepository.java
+++ gyma/src/main/java/com/gyma/gyma/repository/TransactionRepository.java
@@ -3,7 +3,17 @@ package com.gyma.gyma.repository;
 import com.gyma.gyma.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>, JpaSpecificationExecutor<Transaction> {
+    long countByCreatedAtBetween(LocalDate startDate, LocalDate endDate);
+    @Query("SELECT SUM(t.price) FROM Transaction t WHERE t.createdAt BETWEEN :startDate AND :endDate")
+    BigDecimal sumPriceByCreatedAtBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
+    @Query("SELECT t.category FROM Transaction t WHERE t.createdAt BETWEEN :startDate AND :endDate GROUP BY t.category ORDER BY COUNT(t.category) DESC LIMIT 1")
+    String findMostFrequentCategory(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
